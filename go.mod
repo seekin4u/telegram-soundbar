d3b9dae3f81a9c04f2d886407563a98ff174bd27
Max Sauce <wagamama47@gmail.com>
2025-10-31 20:10:03 +0200

    first sound added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/seekin4u/telegram-soudbar
 
 go 1.25.1
 
-require github.com/go-telegram/bot v1.17.0 // indirect
+require (
+	github.com/go-telegram/bot v1.17.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -7,12 +7,18 @@ import (
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using env variables instead")
+	}
+
 	token := os.Getenv("BOT_TOKEN")
 	if token == "" {
-		log.Fatal("Please set BOT_TOKEN environment variable")
+		log.Fatal("BOT_TOKEN not set in .env nor in env")
 	}
 
 	b, err := bot.New(token, bot.WithDefaultHandler(defaultHandler))
@@ -20,8 +26,8 @@ func main() {
 		log.Fatalf("failed to create bot: %v", err)
 	}
 
-	// b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, handleStart)
-	// b.RegisterHandler(bot.HandlerTypeMessageText, "/voice", bot.MatchTypeExact, handleVoice)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, handleStart)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/voice", bot.MatchTypeExact, handleVoice)
 
 	botname, berr := b.GetMe(context.Background())
 	if berr != nil {
@@ -33,8 +39,39 @@ func main() {
 	b.Start(context.Background())
 }
 
+// Default handler (called for any unmatched message)
 func defaultHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	if update.Message != nil {
 		log.Printf("[%s] %s", update.Message.From.Username, update.Message.Text)
+	}
+}
+
+func handleStart(ctx context.Context, b *bot.Bot, update *models.Update) {
+	msg := &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   "Hi! Send /voice to get your voice message 🎙️",
+	}
+	b.SendMessage(ctx, msg)
+	log.Print("User /start")
+}
+
+func handleVoice(ctx context.Context, b *bot.Bot, update *models.Update) {
+	// https://github.com/go-telegram/bot/blob/v1.17.0/models/input_file.go#L15
+	f, err := os.Open("mda-ebat.ogg")
+	if err != nil {
+		log.Printf("failed to open voice file: %v", err)
+		return
+	}
+	defer f.Close()
+
+	voice := &bot.SendVoiceParams{
+		ChatID:  update.Message.Chat.ID,
+		Voice:   &models.InputFileUpload{Filename: "mda-ebat.ogg", Data: f},
+		Caption: "Here’s your voice message!",
+	}
+
+	_, err = b.SendVoice(ctx, voice)
+	if err != nil {
+		log.Printf("failed to send voice: %v", err)
 	}
 }
